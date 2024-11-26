4b7f9a4dba472fa41624bcbec518ad2c786f1110
Adam FOUTIH <adam.foutih@gmail.com>
2024-11-26 16:47:33 +0100

    Park Auto first commit

---
+++ src/main/java/com/parkauto/rest/entity/Vehicule.java
@@ -0,0 +1,24 @@
+package com.parkauto.rest.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "VEHICULE")
+
+public class Vehicule {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "IDVEHICULE")
+	private Long matricule;
+	
+	@Column(name="ANNEEMODEL")
+	private int anneeModel;
+	
+
+}
